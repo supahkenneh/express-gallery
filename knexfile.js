e8bdfa2f43136ff25491c910e534b28e31ce6033
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-06 16:14:30 -1000

    first commit, directories made, set up .js files

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'express_gallery',
+      user:     'da_user',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations'),
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
