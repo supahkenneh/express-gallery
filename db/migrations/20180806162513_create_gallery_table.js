8a871c0ba145bf00e680c8d6e865f4c609f8b799
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-07 18:29:40 -1000

    modified table

--- db/migrations/20180806162513_create_gallery_table.js
+++ db/migrations/20180806162513_create_gallery_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('gallery', table => {
     table.increments();
     table.string('author', 50).notNullable();
-    table.string('link', 255).notNullable();
+    table.text('link').notNullable();
     table.text('description');
     table.timestamps(true, true);
   });
